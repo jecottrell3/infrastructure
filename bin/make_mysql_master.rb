f5a5a6d5851eff3ad2c5fd5626a4ca6354603385
ggabriel <ggabriel@572b5e52-1cbb-c948-9867-ecc5cec4d80e>
2011-02-28 20:42:29 +0000

    Make make_mysql_master.rb work on CentOS.
    git-svn-id: https://svn.microstrategy.com/svn/Infrastructure/trunk@18 572b5e52-1cbb-c948-9867-ecc5cec4d80e

--- bin/make_mysql_master.rb
+++ bin/make_mysql_master.rb
@@ -1,31 +1,33 @@
-# Run this with "ruby -rubygems make_mysql_master.rb host_to_make_master"
-#
+#!/usr/bin/ruby -rubygems
+
 # Gary Gabriel <ggabriel@microstrategy.com>
 
 require "net/ssh"
 require "net/sftp"
 
 unless host = ARGV[0]
-  puts "Usage: ruby -rubygems make_mysql_master.rb host_to_make_master"
+  puts "Usage: #{$0} host_to_make_master"
   exit 1
 end
 
 def stop_mysql(ssh)
-  ssh.exec!("service mysql stop")
+  ssh.exec!("/databases/mysql/bin/mysql.stop")
   sleep 1
 end
 
 def start_mysql(ssh)
-  ssh.exec!("service mysql start")
+  ssh.exec!("/databases/mysql/bin/mysql.start")
 end
 
-def write_master_mycnf(sftp)
-  local_mycnf = [ "[mysqld]\n",
-                  "log-bin = master\n",
-                  "binlog_format = ROW\n"
-                ].join
-  sftp.file.open("/etc/mysql/conf.d/mstr_master.cnf", "w") do |f|
-    f.write(local_mycnf)
+def write_master_mycnf(ssh)
+  ssh.sftp.connect do |sftp|
+    local_mycnf = [ "[mysqld]",
+                    "log-bin = master",
+                    "binlog_format = ROW"
+                  ].map { |x| x + "\n" }.join
+    sftp.file.open("/databases/mysql/etc/conf.d/master.cnf", "w") do |f|
+      f.write(local_mycnf)
+    end
   end
 end
 
@@ -33,10 +35,8 @@ Net::SSH.start(host, "root") do |ssh|
   puts "Stopping MySQL"
   stop_mysql(ssh)
 
-  ssh.sftp.connect do |sftp|
-    puts "Making this MySQL a master"
-    write_master_mycnf(sftp)
-  end
+  puts "Making this MySQL a master"
+  write_master_mycnf(ssh)
 
   puts "Starting MySQL"
   start_mysql(ssh)
