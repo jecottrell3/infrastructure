223d241902833bd0cea74eb8e7048aa01b9d1789
ggabriel <ggabriel@572b5e52-1cbb-c948-9867-ecc5cec4d80e>
2011-03-23 20:27:26 +0000

    Add support for multiple mysql instances per host.
    git-svn-id: https://svn.microstrategy.com/svn/Infrastructure/trunk@45 572b5e52-1cbb-c948-9867-ecc5cec4d80e

--- bin/make_mysql_master.rb
+++ bin/make_mysql_master.rb
@@ -6,40 +6,45 @@ require "net/ssh"
 require "net/sftp"
 
 unless host = ARGV[0]
-  puts "Usage: #{$0} host_to_make_master"
+  puts "Usage: #{$0} host_to_make_master [<port>]"
+  puts "       default port is 3306"
   exit 1
 end
 
-def stop_mysql(ssh)
-  ssh.exec!("/databases/mysql/bin/mysql.stop")
+port = ARGV[1] ? ARGV[1].to_i : 3306
+
+def stop_mysql(ssh, mysql_root)
+  ssh.exec!("#{mysql_root}/mysql/bin/mysql.stop")
   sleep 1
 end
 
-def start_mysql(ssh)
-  ssh.exec!("/databases/mysql/bin/mysql.start")
+def start_mysql(ssh, mysql_root)
+  ssh.exec!("#{mysql_root}/mysql/bin/mysql.start")
 end
 
-def write_master_mycnf(ssh)
+def write_master_mycnf(ssh, mysql_root)
   ssh.sftp.connect do |sftp|
     local_mycnf = [ "[mysqld]",
                     "log-bin = master",
                     "binlog_format = ROW"
                   ].map { |x| x + "\n" }.join
-    sftp.file.open("/databases/mysql/etc/conf.d/master.cnf", "w") do |f|
+    sftp.file.open("#{mysql_root}/mysql/etc/conf.d/master.cnf", "w") do |f|
       f.write(local_mycnf)
     end
   end
 end
 
 Net::SSH.start(host, "root") do |ssh|
+  mysql_root = "/MSTR/mysql#{port}"
+
   puts "Stopping MySQL"
-  stop_mysql(ssh)
+  stop_mysql(ssh, mysql_root)
 
   puts "Making this MySQL a master"
-  write_master_mycnf(ssh)
+  write_master_mycnf(ssh, mysql_root)
 
   puts "Starting MySQL"
-  start_mysql(ssh)
+  start_mysql(ssh, mysql_root)
 
   puts "Done."
 end
