7ae90924dc1387080739f93a6fe4e45b79f32a43
Gary Gabriel <ggabriel@microstrategy.com>
2011-05-16 16:45:43 -0400

    remove update heartbeat event

--- mysql/mon_update_heartbeat_event.sql
+++
@@ -1,6 +0,0 @@
-DROP EVENT IF EXISTS mon.event_update_heartbeat;
-
-CREATE EVENT mon.event_update_heartbeat
-ON SCHEDULE EVERY 1 SECOND STARTS NOW() + INTERVAL 1 SECOND
-DO UPDATE mon.Heartbeat SET heartbeat = NOW() LIMIT 1;
-
