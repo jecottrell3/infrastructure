e900ccaff32d8ed7fa522aa287868c903ecbddfd
Gary Gabriel <ggabriel@microstrategy.com>
2011-05-23 13:11:04 -0400

    Make the output suitable for spreadsheets.

--- bin/sma_wh_data_sizes.php
+++ bin/sma_wh_data_sizes.php
@@ -6,35 +6,57 @@ $num_shards = 5;
 $oltp_tables = array('Album', 'Application', 'Checkin', 'Comment_Likes', 'Crawl_Info', 'Event', 'Event_Resp_Status', 'Event_Visibility', 'FBCall', 'FBGroup', 'FBObject', 'FBUser', 'Friendlist', 'Friendlist_has_User', 'Group_has_User', 'Likes', 'Link', 'Location', 'Member_Crawl_Records', 'Note', 'Object_Comment', 'Object_Likes', 'Object_Privacy_Detail', 'Object_Tag', 'Object_Visibility', 'Page', 'Photo', 'Post', 'Profile', 'StatusMessage', 'User_Daily_Data_Amount', 'User_Friend_Info', 'User_Page_Info', 'Video', 'photo_tag', 'Member_Crawl_Records', 'Member_Management');
 $geo_tables = array('GEODATASOURCE_CITIES_TITANIUM', 'GEODATASOURCE_COUNTRIES');
 
-$all_users = array();
+echo "\tTotal Users\tOLTP Data (MB)\tOLTP Index(MB)\tETL Data (MB)\tETL Index (MB)\tTotal DB - OLTP & ETL (MB)\n";
+$total_num_users = 0;
+$total_oltp_data_size_mb = 0.0;
+$total_oltp_index_size_mb = 0.0;
+$total_geo_data_size_mb = 0.0;
+$total_geo_index_size_mb = 0.0;
+$total_etl_data_size_mb = 0.0;
+$total_etl_index_size_mb = 0.0;
+
 for($shard = 1; $shard <= $num_shards; $shard++) {
-  echo "Shard $shard\n";
   $conn = mysql_connect("s${shard}db-master.prod.wisdom.com", "crawl", "crawl9279");
 
   $res = mysql_query("SELECT COUNT(1) FROM sma_wh.Member_Management WHERE ShardID = $shard", $conn);
   $row = mysql_fetch_row($res);
-  echo "number of users\t" . $row[0] . "\n";
+  $num_users = (int)$row[0];
+  $total_num_users += $num_users;
   mysql_free_result($res);
 
   $res = mysql_query("SELECT SUM(data_length) / (1024 * 1024), SUM(index_length) / (1024 * 1024) FROM information_schema.tables WHERE table_schema = 'sma_wh' AND table_name IN ('" . implode("', '", $oltp_tables) . "')", $conn);
   $row = mysql_fetch_row($res);
-  echo "OLTP data size in MB\t" . $row[0] . "\n";
-  echo "OLTP index size in MB\t" . $row[0] . "\n";
+  $oltp_data_size_mb = (double)$row[0];
+  $oltp_index_size_mb = (double)$row[1];
+  $total_oltp_data_size_mb += $oltp_data_size_mb;
+  $total_oltp_index_size_mb += $oltp_index_size_mb;
   mysql_free_result($res);
 
   $res = mysql_query("SELECT SUM(data_length) / (1024 * 1024), SUM(index_length) / (1024 * 1024) FROM information_schema.tables WHERE table_schema = 'sma_wh' AND table_name IN ('" . implode("', '", $geo_tables) . "')", $conn);
   $row = mysql_fetch_row($res);
-  echo "GEO data size in MB\t" . $row[0] . "\n";
-  echo "GEO index size in MB\t" . $row[0] . "\n";
+  $geo_data_size_mb = (double)$row[0];
+  $geo_index_size_mb = (double)$row[1];
+  $total_geo_data_size_mb += $oltp_data_size_mb;
+  $total_geo_index_size_mb += $oltp_index_size_mb;
   mysql_free_result($res);
 
   $res = mysql_query("SELECT SUM(data_length) / (1024 * 1024), SUM(index_length) / (1024 * 1024) FROM information_schema.tables WHERE table_schema = 'sma_wh' AND table_name NOT IN ('" . implode("', '", array_merge($oltp_tables, $geo_tables)) . "')", $conn);
   $row = mysql_fetch_row($res);
-  echo "ETL data size in MB\t" . $row[0] . "\n";
-  echo "ETL index size in MB\t" . $row[0] . "\n";
+  $etl_data_size_mb = (double)$row[0];
+  $etl_index_size_mb = (double)$row[1];
+  $total_etl_data_size_mb += $oltp_data_size_mb;
+  $total_etl_index_size_mb += $oltp_index_size_mb;
   mysql_free_result($res);
 
   mysql_close($conn);
+
+  printf("Shard %d\t%d\t%1.0f\t%1.0f\t%1.0f\t%1.0f\t%1.0f\n", $shard, $num_users,
+         $oltp_data_size_mb, $oltp_index_size_mb, $etl_data_size_mb, $etl_index_size_mb,
+         ($oltp_data_size_mb + $oltp_index_size_mb + $etl_data_size_mb + $etl_index_size_mb));
 }
+
+printf("Totals\t%d\t%1.0f\t%1.0f\t%1.0f\t%1.0f\t%1.0f\n", $total_num_users,
+         $total_oltp_data_size_mb, $total_oltp_index_size_mb, $total_etl_data_size_mb, $total_etl_index_size_mb,
+         ($total_oltp_data_size_mb + $total_oltp_index_size_mb + $total_etl_data_size_mb + $total_etl_index_size_mb));
 
 ?>
