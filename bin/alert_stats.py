70d63730e400862c03518283592060160464b687
Gary Gabriel <ggabriel@microstrategy.com>
2011-06-09 09:43:53 -0400

    Take user/password, check table/index sizes.

--- bin/alert_stats.py
+++ bin/alert_stats.py
@@ -3,14 +3,15 @@
 import sys
 import getopt
 import MySQLdb
+import MySQLdb.cursors
 
 def usage():
-  return "Usage: %s (-h|--host) <host> (-P|--port) <port>" % sys.argv[0]
+  return "Usage: %s (-h|--host) <host> (-P|--port) <port> (-u|--user) <user> (-p|--password) <password>" % sys.argv[0]
 
 def parse_args(args):
   try:
-    opts, args = getopt.getopt(args, "h:P:", ["host=", "port="])
-    host, port = None, None
+    opts, args = getopt.getopt(args, "h:P:u:p:", ["host=", "port=", "user=", "password="])
+    host, port, user, password = None, None, None, None
     for o, a in opts:
       if o in ("-h", "--host"):
         host = a
@@ -19,27 +20,40 @@ def parse_args(args):
           port = int(a)
         except ValueError:
           raise getopt.GetoptError, "port must be an integer"
-    if not host or not port:
-      raise getopt.GetoptError, "host and port are required"
+      elif o in ("-u", "--user"):
+        user = a
+      elif o in ("-p", "--password"):
+        password = a
+    if host == None or port == None or user == None or password == None:
+      raise getopt.GetoptError, "host, port, user and password are required"
   except getopt.GetoptError, err:
     print usage()
     print str(err)
     sys.exit(3)
-  return host, port
+  return host, port, user, password
 
 def main():
-  host, port = parse_args(sys.argv[1:])
+  host, port, user, password = parse_args(sys.argv[1:])
   try:
-    conn = MySQLdb.connect(host=host, user="mon", port=port)
+    conn = MySQLdb.connect(host=host, port=port, user=user, passwd=password, cursorclass=MySQLdb.cursors.SSCursor)
   except MySQLdb.OperationalError, err:
     sys.exit(1)
+
   cursor = conn.cursor()
   cursor.execute("SELECT COUNT(uid) FROM alert.alertUser")
   row = cursor.fetchone()
-  num_users = row[0]
+  num_users = int(row[0])
   cursor.close()
+
+  cursor = conn.cursor()
+  cursor.execute("SELECT SUM(data_length) / (1024 * 1024), SUM(index_length) / (1024 * 1024) FROM information_schema.tables WHERE table_schema = 'alert'")
+  row = cursor.fetchone()
+  data_size = float(row[0])
+  index_size = float(row[1])
+  cursor.close()
+
   conn.close()
-  print "|num_alert_users=%d" % num_users
+  print "|num_alert_users=%d data_size_mb=%1.3f index_size_mb=%1.3f" % (num_users, data_size, index_size)
 
 if __name__ == "__main__":
   main()
